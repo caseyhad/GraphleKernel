b47f01b98bb999a0576e1b487d8cff7301ff4c56
caseyhad <118768448+caseyhad@users.noreply.github.com>
2023-01-10 13:44:39 -0800

    Add files via upload

---
+++ buildtree.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.19.19
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 325ba270-912f-11ed-1604-e9b672241b99
+
+
+# ╔═╡ Cell order:
+# ╠═325ba270-912f-11ed-1604-e9b672241b99
